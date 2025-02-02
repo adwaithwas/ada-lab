a4274023743f89e3308696fc53a4acc1ebb053b0
adwaithwas <betteradwaith@gmail.com>
2025-02-02 16:24:28 +0530

    imporved SortUtils and added merge sort algorithm

---
+++ sorting_algos/SortUtils.java
@@ -0,0 +1,42 @@
+package sorting_algos;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class SortUtils {
+    public static int[] getRandomArray(int n, int bound){
+        int[] arr = new int[n];
+        Random rand = new Random();
+
+        for(int i=0; i<n; i++){
+            arr[i] = rand.nextInt(bound);
+        }
+
+        return arr;
+    }
+
+    public static void printArray(int[] arr){
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void tellTime(long startTime, long endTime, char unit){
+        long timeTaken = endTime - startTime;
+        unit = Character.toLowerCase(unit);
+        
+        switch(unit){
+            case 'n':
+                System.out.println("Time Taken: " + timeTaken + " nano seconds");
+                break;
+            
+            case 'm':
+                System.out.println("Time Taken: " + timeTaken/ 1_000_000.0 + " milli seconds");
+                break;
+
+            case 's':
+                System.out.println("Time Taken: " + timeTaken/1_000_000_000.0 + " seconds");
+                break;
+            default:
+                System.out.println("Time Taken: " + timeTaken + " nano seconds");
+        }
+    }
+}
---
+++ sorting_algos/readme.txt
@@ -0,0 +1,2 @@
+javac .\sorting_algos\RandomUtils.java .\sorting_algos\MergeSort.java
+java sorting_algos.MergeSort
