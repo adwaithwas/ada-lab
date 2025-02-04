a4b73c285618d89972befb51fdf750bead70a298
adwaithwas <betteradwaith@gmail.com>
2025-02-05 05:25:55 +0530

    test

--- sorting_algos/test.java
+++ sorting_algos/test.java
@@ -1,27 +1,33 @@
 package sorting_algos;
+import utils.*;;
 
-import utils.SortUtils;
-
-public class test {
+public class test{
     public static void main(String[] args) {
-        int[] arr = SortUtils.getRandomArray(20, 100);
+        int[] arr = SortUtils.getRandomArray(10, 50);
+        
+        SortUtils.printArray(arr);  //un-sorted array
 
-        SortUtils.printArray(arr); //printing unsorted array
-
+        long start = System.nanoTime();
         mergeSort(arr);
+        long end= System.nanoTime();
+        
+        SortUtils.printArray(arr);  //sorted array
 
-        SortUtils.printArray(arr); //printing sorted array
+        SortUtils.tellTime(start, end, 'm');
     }
 
     private static void mergeSort(int[] arr){
         int length = arr.length;
-        if (length <= 1) return;
 
-        int middle = arr.length/2;
+        if(length<=1){
+            return;
+        }
+
+        int middle = length/2;
         int[] larr = new int[middle];
         int[] rarr = new int[length-middle];
 
-        int i = 0; int j=0;
+        int i =0, j =0;
         for(; i<length; i++){
             if(i<middle){
                 larr[i] = arr[i];
@@ -31,30 +37,32 @@ public class test {
             }
         }
         mergeSort(larr); mergeSort(rarr);
-        merge(larr, rarr, arr);
+        merge(arr, larr, rarr);
     }
 
-    private static void merge(int[] larr, int[] rarr, int[] arr){
-        int i = 0, l = 0, r = 0;
-        int lSize = arr.length/2;
-        int rSize = arr.length-lSize;
+    private static void merge(int[] arr, int[] larr, int[] rarr){
+            int size = arr.length;
+            int lsize = size/2;
+            int rsize = size - lsize;
 
-        while(l < lSize && r < rSize){
-            if(larr[l] < rarr[r]){
-                arr[i] = larr[l];
-                i++; l++;
-            } else {
-                arr[i] = rarr[r];
-                i++; r++;
-            }        
-        }
-        while(l<lSize){
-            arr[i] = larr[l];
-            i++; l++;
-        }   
-        while(r<rSize){
-            arr[i] = rarr[r];
-            i++; r++;
-        } 
-}
+            int i=0, j=0, k=0;
+
+            while(j<lsize && k<rsize){
+                if(larr[j] < rarr[k]){
+                    arr[i] = larr[j];
+                    j++; i++;
+                } else {
+                    arr[i] = rarr[k];
+                    k++; i++;
+                }
+            }
+            while(j<lsize){
+                arr[i] = larr[j];
+                j++; i++;
+            }
+            while(k<rsize){
+                arr[i] = rarr[k];
+                k++; i++;
+            }
+    }
 }
